b0c231a7337d86c839e1027fc3ae1bc1b445c0c8
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-25 22:43:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,12 +3,8 @@ counter = 0
   while  counter < words.length
   yield(words[counter])
   counter += 1
-   # words do |i|
      # i
      end
      words
-  # while i == 4
- # 
- # end
   # code here
 end
